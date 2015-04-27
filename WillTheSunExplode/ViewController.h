72d06bd35da6f99fcbfbd84075708408e0f3eb50
Keaton Burleson <keaton.burleson@me.com>
2015-04-27 13:29:35 -0500

    Initial Commit

---
+++ WillTheSunExplode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WillTheSunExplode
+//
+//  Created by Keaton Burleson on 4/27/15.
+//  Copyright (c) 2015 Keaton Burleson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
